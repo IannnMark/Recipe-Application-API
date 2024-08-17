34e837de419270cb640242c507ca8378d9695c34
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-17 11:44:52 +0800

    user signup controller

---
+++ controllers/authController.js
@@ -0,0 +1,16 @@
+const User = require("../models/user");
+const bcryptjs = require("bcryptjs");
+
+
+exports.signUp = async (req, res, next) => {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+
+    try {
+        await newUser.save();
+        res.status(201).json('User created successfully');
+    } catch (error) {
+        next(error);
+    }
+};
