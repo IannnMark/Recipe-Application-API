796c3695cf3fd826ff2ef9ae7616de082b908194
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-17 11:30:16 +0800

    create a user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    avatar: {
+        type: String,
+        default: "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
+    },
+}, { timestamps: true })
+
+module.exports = mongoose.model("User", userSchema);
