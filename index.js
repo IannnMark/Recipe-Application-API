7960227ae12e339cf80c0deedf3122027588a2a2
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-17 20:34:42 +0800

    user routes

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoutes = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 
+dotenv.config();
+
+mongoose.connect(process.env.DATABASE).then(() => {
+    console.log("Connected to MongoDB");
+})
+    .catch(err => {
+        console.log(err);
+    });
 
 const app = express();
-
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
     console.log('Server listening on port 3000');
 }
 );
+
+app.use('/api/auth', authRoutes);
+
+
+//Middleware to handle the possible errors
+
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal server error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
