c5dd0a501b90b482b57e8db0fe1ce17d0dd257a4
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-18 14:48:42 +0800

    user signIn controller

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const bcryptjs = require("bcryptjs");
+const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 
 exports.signUp = async (req, res, next) => {
@@ -14,3 +16,23 @@ exports.signUp = async (req, res, next) => {
         next(error);
     }
 };
+
+exports.signIn = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(new errorHandler(404, "User not found"));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(new errorHandler(401, "Wrong credentials"));
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
+        //destructuring yung password para hindi makita kapag nag test sa backend 
+        const { password: pass, ...rest } = validUser._doc;
+        res
+            .cookie('access_token', token, { httpOnly: true })
+            .status(200)
+            .json(rest);
+    } catch (error) {
+        next(error);
+    }
+};;
