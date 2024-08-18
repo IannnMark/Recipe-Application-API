1db97a9d32e0740cb2e7b58f35ed075090846369
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-18 14:53:08 +0800

    user signOut route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
 const express = require("express");
-const { signUp, signIn } = require("../controllers/authController");
+const { signUp, signIn, signOut } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
+router.get("/signout", signOut);
 
 
 
