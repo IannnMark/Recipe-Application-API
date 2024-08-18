af643c912a71912a34ece453fb6b6925cc2c1ea2
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-18 14:52:51 +0800

    user signOut

--- controllers/authController.js
+++ controllers/authController.js
@@ -35,4 +35,13 @@ exports.signIn = async (req, res, next) => {
     } catch (error) {
         next(error);
     }
-};;
+};
+
+exports.signOut = async (req, res, next) => {
+    try {
+        res.clearCookie('access_token');
+        res.status(200).json("User has been logged out");
+    } catch (error) {
+        next(error);
+    }
+}
