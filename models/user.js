7d18121b55722e202c86f81105bedb7c9815dd49
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-18 14:21:42 +0800

    add role

--- models/user.js
+++ models/user.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
         type: String,
         default: "https://static.vecteezy.com/system/resources/previews/005/544/718/non_2x/profile-icon-design-free-vector.jpg"
     },
+    role: {
+        type: String,
+        default: "user",
+    },
 }, { timestamps: true })
 
 module.exports = mongoose.model("User", userSchema);
