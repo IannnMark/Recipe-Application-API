c53f535bd7785b6e86a721665884bd0fab03a7a7
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-22 11:10:48 +0800

    google route

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { signUp, signIn, signOut } = require("../controllers/authController");
+const { signUp, signIn, signOut, google } = require("../controllers/authController");
 
 const router = express.Router();
 
 router.post("/signup", signUp);
 router.post("/signin", signIn);
 router.get("/signout", signOut);
+router.post("/google", google);
+
 
 
 
