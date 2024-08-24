d1377b05b6f3e2ca9e21b051d9a346dc22b57640
ianmark8 <ianmarkmorga07@gmail.com>
2024-08-24 13:29:46 +0800

    create the userRoutes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const authRoutes = require("./routes/auth");
+const userRoutes = require("./routes/user");
 const cookieParser = require("cookie-parser");
 
 dotenv.config();
@@ -22,7 +23,8 @@ app.listen(3000, () => {
 }
 );
 
-app.use('/api/auth', authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 
 
 //Middleware to handle the possible errors
