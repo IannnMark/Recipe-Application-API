8d112999b39f516af3df84705922ee08495ac981
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-04 20:33:32 +0800

    add a console log in create recipe

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -4,6 +4,7 @@ const errorHandler = require("../utils/error");
 exports.createRecipe = async (req, res, next) => {
     try {
         const recipe = await Recipe.create(req.body);
+        // console.log("Save Recipe", recipe);
         return res.status(201).json(recipe);
     } catch (error) {
         next(error);
