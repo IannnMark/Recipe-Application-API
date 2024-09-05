ed81c6330c13ad1bc59348996ecb2fb2826cea5e
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-05 14:41:26 +0800

    add the get user recipe route

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 const express = require("express");
-const { updateUser, deleteUser } = require("../controllers/userController");
+const { updateUser, deleteUser, getUserRecipe } = require("../controllers/userController");
 const verifyToken = require("../utils/verifyUser");
 
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/recipe/:id", verifyToken, getUserRecipe);
 
 
 
