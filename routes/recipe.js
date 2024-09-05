b3de340e78be215c3c3279d9c5ddb85a0f0f5eee
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-05 14:13:03 +0800

    get recipe route

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
-const { createRecipe } = require("../controllers/recipeController");
+const { createRecipe, getRecipe } = require("../controllers/recipeController");
 
 router.post("/create", verifyToken, createRecipe);
+router.get("/recipe", verifyToken, getRecipe);
 
 
 module.exports = router;
