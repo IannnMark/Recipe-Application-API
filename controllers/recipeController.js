547cf3fa161b8e28df27eff91d62fd4859348b26
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-05 14:12:18 +0800

    get recipe controller

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -10,3 +10,15 @@ exports.createRecipe = async (req, res, next) => {
         next(error);
     }
 }
+
+exports.getRecipe = async (req, res, next) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return next(errorHandler(404, "Recipe not found"));
+        }
+        res.status(200).json(recipe);
+    } catch (error) {
+        next(error);
+    }
+}
