4a120cb7d6e18030d053a2b6fc077e730566f351
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-07 00:13:33 +0800

    delete recipe route

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
-const { createRecipe, getRecipe } = require("../controllers/recipeController");
+const { createRecipe, getRecipe, deleteRecipe, updateRecipe } = require("../controllers/recipeController");
 
 router.post("/create", verifyToken, createRecipe);
 router.get("/get/:id", getRecipe);
+router.delete("/delete/:id", verifyToken, deleteRecipe);
+// router.put("/update/:id", verifyToken, updateRecipe);
 
 
 module.exports = router;
