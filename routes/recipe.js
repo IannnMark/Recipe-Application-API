afe26925151773478b1859fe2ef6074f0e94e46f
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-07 00:38:54 +0800

    add the search route

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const verifyToken = require("../utils/verifyUser");
 const router = express.Router();
-const { createRecipe, getRecipe, deleteRecipe, updateRecipe } = require("../controllers/recipeController");
+const { createRecipe, getRecipe, deleteRecipe, updateRecipe, getRecipes } = require("../controllers/recipeController");
 
 router.post("/create", verifyToken, createRecipe);
 router.get("/get/:id", getRecipe);
 router.delete("/delete/:id", verifyToken, deleteRecipe);
 router.put("/update/:id", verifyToken, updateRecipe);
+router.get("/get", getRecipes);
 
 
 module.exports = router;
